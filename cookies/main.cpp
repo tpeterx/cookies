357db2f49d95235478c7ca34c7312d513ce8ab76
Todd Peterson <tpeterx@gmail.com>
2017-05-24 17:45:27 -0500

    Initial Commit

---
+++ cookies/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cookies
+//
+//  Created by Todd Peterson on 5/24/17.
+//  Copyright © 2017 Todd Peterson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
